bb55938fd994dd76fff3f7141d205cf02caaeaee
fikus69 <kharuno06@bk.ru>
2024-03-06 09:52:07 +1000

    EWEW

---
+++ Baraxolka/Operativka.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baraxolka
+{
+    public class Operativka
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        //public byte[] Image { get; set; }
+    }
+}
